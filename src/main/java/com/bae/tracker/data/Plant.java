ae56d3db6b03c03cef5aaccb9ab3c99999704ea1
victoriamanicom <victoriamanicom@gmail.com>
2021-07-27 16:21:50 +0100

    PT-19 #comment Added Entity to Plant class

--- src/main/java/com/bae/tracker/data/Plant.java
+++ src/main/java/com/bae/tracker/data/Plant.java
@@ -1,8 +1,19 @@
 package com.bae.tracker.data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Plant {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	@Column
 	private String name;
 	private int potSize;
 	private String leafColour;
